b9b5a53e7fe48f3d92fbef26945670c622c35a12
Faris MS <faris@q5soft.com>
2025-09-17 05:56:18 +0530

    test commit

---
+++ test/widget_test.dart
@@ -0,0 +1,19 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:sales_bets/main.dart';
+
+void main() {
+  testWidgets('App structure test', (WidgetTester tester) async {
+    // Test that the app can be created without errors
+    const app = SalesBetsApp();
+    expect(app, isA<StatelessWidget>());
+  });
+}
